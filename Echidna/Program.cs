7b55f7f7d8f27017006ead68a74e833806a71c18
Jade Wright <iamjwright159@gmail.com>
2023-08-26 17:45:33 -0400

    Draw a triangle

--- Echidna/Program.cs
+++ Echidna/Program.cs
@@ -1,3 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Drawing;
+using OpenTK.Graphics.ES30;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 
-Console.WriteLine("Hello, World!");
+namespace Echidna;
+
+public class Program : GameWindow
+{
+	private static void Main()
+	{
+		using Program window = new();
+		window.Run();
+	}
+	
+	private int vertexBufferObject;
+	private int vertexArrayObject;
+	
+	private Shader? shader;
+	
+	private readonly float[] vertices =
+	{
+		-0.5f, -0.5f, 0.0f, // Bottom-left vertex
+		0.5f, -0.5f, 0.0f, // Bottom-right vertex
+		0.0f,  0.5f, 0.0f  // Top vertex
+	};
+    
+	private Program() : base(GameWindowSettings.Default, new NativeWindowSettings { Size = (1080, 720), Title = "bepis" }) { }
+	
+	protected override void OnLoad()
+	{
+		base.OnLoad();
+		
+		GL.ClearColor(0, 0, 0, 1);
+		
+		vertexBufferObject = GL.GenBuffer();
+		GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBufferObject);
+		GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+		
+		vertexArrayObject = GL.GenVertexArray();
+		GL.BindVertexArray(vertexArrayObject);
+		GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+		GL.EnableVertexAttribArray(0);
+		
+		shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+		shader.Bind();
+	}
+	
+	protected override void OnUpdateFrame(FrameEventArgs args)
+	{
+		base.OnUpdateFrame(args);
+		
+		if (KeyboardState.IsKeyDown(Keys.Escape))
+			Close();
+	}
+	
+	protected override void OnRenderFrame(FrameEventArgs args)
+	{
+		base.OnRenderFrame(args);
+		
+		shader!.Bind();
+		GL.BindVertexArray(vertexArrayObject);
+		GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+		SwapBuffers();
+	}
+	
+	protected override void OnResize(ResizeEventArgs args)
+	{
+		base.OnResize(args);
+		
+		GL.Viewport((Size)Size);
+	}
+	
+	protected override void OnUnload()
+	{
+		base.OnUnload();
+		
+		shader!.Dispose();
+	}
+}
---
+++ Echidna/Shader.cs
@@ -0,0 +1,66 @@
+﻿using OpenTK.Graphics.ES30;
+
+namespace Echidna;
+
+public class Shader : IDisposable
+{
+	private readonly int handle;
+	private bool disposed;
+	
+	public Shader(string vertexPath, string fragmentPath)
+	{
+		int vertexShader = CompileShader(vertexPath, ShaderType.VertexShader);
+		int fragmentShader = CompileShader(fragmentPath, ShaderType.FragmentShader);
+		
+		handle = GL.CreateProgram();
+		GL.AttachShader(handle, vertexShader);
+		GL.AttachShader(handle, fragmentShader);
+		GL.LinkProgram(handle);
+		
+		GL.GetProgram(handle, GetProgramParameterName.LinkStatus, out int success);
+		if (success == 0)
+		{
+			string infoLog = GL.GetProgramInfoLog(handle);
+			Console.WriteLine(infoLog);
+		}
+	}
+	
+	~Shader()
+	{
+		if (!disposed)
+			Console.WriteLine("GPU Resource leak! Did you forget to call Dispose()?");
+	}
+	
+	private static int CompileShader(string path, ShaderType type)
+	{
+		string source = File.ReadAllText(path);
+		
+		int shader = GL.CreateShader(type);
+		GL.ShaderSource(shader, source);
+		GL.CompileShader(shader);
+		
+		GL.GetShader(shader, ShaderParameter.CompileStatus, out int success);
+		if (success == 0)
+		{
+			string infoLog = GL.GetShaderInfoLog(shader);
+			Console.WriteLine(infoLog);
+		}
+		
+		return shader;
+	}
+	
+	public void Bind()
+	{
+		GL.UseProgram(handle);
+	}
+	
+	public void Dispose()
+	{
+		if (!disposed)
+		{
+			GL.DeleteProgram(handle);
+			disposed = true;
+		}
+		GC.SuppressFinalize(this);
+	}
+}
